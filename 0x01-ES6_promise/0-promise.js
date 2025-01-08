3047fd8605da10868d2374f232348b5b9c334f59
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2025-01-08 12:20:17 +0000

    promise filr

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,7 @@
-function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    // You can add any asynchronous operation here
-    resolve('Success');
-  });
+function getResponseFromAPI()	{
+	return new Promise((resolve) =>	{
+		// You can add any asynchronous operation here
+		resolve('Success');
+	});
 }
-
 export default getResponseFromAPI;
-
