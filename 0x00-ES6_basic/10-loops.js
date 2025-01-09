1e0fffe2b2a02273d2ec4820c1543e7cdffc03ed
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2025-01-09 09:53:23 +0000

    my last push

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,16 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const array = [];
+  // Create a new reference to the same array
+  const newArray = array;
+
+  // Iterate through each value in the array using a for...of loop
   for (const value of array) {
-    array.push(appendString + value);
+    // Get the index of the current value in the original array
+    const idx = array.indexOf(value);
+
+    // Modify the corresponding element in the new array by appending the specified string
+    newArray[idx] = appendString + value;
   }
 
+  // Return the original array (not the new array)
   return array;
 }
